f58f2fe95b36c0b166198279e3a56e3fefb91d03
Alexandr <ak4raulov@yandex.ru>
2022-01-28 17:36:27 +0300

    модальное окно

---
+++ src/js/modules/modals.js
@@ -0,0 +1,41 @@
+const modals = () => {
+	function bindModal(triggerSelector, modalSelector, closeSelector) {
+		const
+			trigger = document.querySelectorAll(triggerSelector),
+			modal = document.querySelector(modalSelector),
+			close = document.querySelector(closeSelector);
+		
+		trigger.forEach((item) => {
+			item.addEventListener('click', (e) => {
+				e.preventDefault();
+				modal.style.display = 'block';
+				document.body.style.overflow = 'hidden';
+			})
+		})
+
+		close.addEventListener('click', () => {
+			modal.style.display = 'none';
+			document.body.style.overflow = '';
+		})
+
+		modal.addEventListener('click', (e) => {
+			if (e.target === modal) {
+				modal.style.display = 'none';
+				document.body.style.overflow = '';
+			}
+		})
+	}
+
+	function showModalByTime(selector, time) {
+		setTimeout(function () {
+			document.querySelector(selector).style.display = 'block';
+			document.body.style.overflow = 'hidden';
+		}, time);
+	}
+
+	bindModal('.header_btn', '.popup_engineer', '.popup_engineer .popup_close');
+	bindModal('.phone_link', '.popup', '.popup_close');
+	showModalByTime('.popup', 6000);
+}
+
+export default modals;
